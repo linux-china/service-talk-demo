264503fe04192e652324e88eb65b9c3b8017cd57
linux_china <libing.chen@gmail.com>
2019-11-06 11:11:10 -0800

    Polishing

--- src/main/java/org/mvnsearch/servicetalk/grpc/HelloGrpcServer.java
+++ src/main/java/org/mvnsearch/servicetalk/grpc/HelloGrpcServer.java
@@ -1,32 +1,20 @@
 package org.mvnsearch.servicetalk.grpc;
 
-import io.grpc.examples.helloworld.Greeter.GreeterService;
 import io.grpc.examples.helloworld.Greeter.ServiceFactory;
-import io.grpc.examples.helloworld.HelloReply;
-import io.grpc.examples.helloworld.HelloRequest;
-import io.servicetalk.concurrent.api.Single;
-import io.servicetalk.grpc.api.GrpcServiceContext;
 import io.servicetalk.grpc.netty.GrpcServers;
 
-import static io.servicetalk.concurrent.api.Single.succeeded;
-
 /**
- * hello grpc Server
+ * hello gRPC Server
  *
  * @author linux_china
  */
 public class HelloGrpcServer {
+
     public static void main(String[] args) throws Exception {
+        ServiceFactory[] serviceFactories = new ServiceFactory[]{new ServiceFactory(new MyGreeterService())};
         GrpcServers.forPort(50051)
-                .listenAndAwait(new ServiceFactory(new MyGreeterService()))
+                .listenAndAwait(serviceFactories)
                 .awaitShutdown();
     }
 
-    private static final class MyGreeterService implements GreeterService {
-
-        @Override
-        public Single<HelloReply> sayHello(final GrpcServiceContext ctx, final HelloRequest request) {
-            return succeeded(HelloReply.newBuilder().setMessage("Hello " + request.getName()).build());
-        }
-    }
 }
